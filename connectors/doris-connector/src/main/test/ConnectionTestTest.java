51c47963bcd6b84e7cecd2eb08e9a1403371f307
longshaofeng <345064577@qq.com>
2022-03-23 21:27:50 +0800

    create TapTable structure in DiscoverSchemaTest

--- connectors/doris-connector/src/main/test/ConnectionTestTest.java
+++ connectors/doris-connector/src/main/test/ConnectionTestTest.java
@@ -1,7 +1,9 @@
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.tapdata.connector.doris.utils.DorisConfig;
 import io.tapdata.pdk.apis.common.DefaultMap;
+import io.tapdata.pdk.apis.utils.ImplementationUtils;
+import io.tapdata.pdk.apis.utils.TapUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
@@ -15,6 +17,7 @@ import java.sql.*;
 @DisplayName("Tests for connection test")
 public class ConnectionTestTest {
     private DorisConfig dorisConfig;
+    private final TapUtils tapUtils = ImplementationUtils.getTapUtils();
 
     @Test
     @DisplayName("Test method connectionTest")
@@ -32,16 +35,15 @@ public class ConnectionTestTest {
 
 
         String json = "{\"after\":{\"id\":1.0,\"description\":\"description123\",\"name\":\"name123\",\"age\":12.0},\"table\":{\"id\":\"empty-table1\",\"name\":\"empty-table1\",\"nameFieldMap\":{\"id\":{\"name\":\"id\",\"originType\":\"VARCHAR\",\"partitionKeyPos\":1,\"pos\":1,\"primaryKey\":true},\"description\":{\"name\":\"description\",\"originType\":\"TEXT\",\"pos\":2},\"name\":{\"name\":\"name\",\"originType\":\"VARCHAR\",\"pos\":3},\"age\":{\"name\":\"age\",\"originType\":\"DOUBLE\",\"pos\":4}}},\"time\":1647660346515}";
-        DefaultMap defaultMap = JSON.parseObject(json, DefaultMap.class);
-        JSONObject after_json_obj = (JSONObject) defaultMap.get("after");
-        DefaultMap after_map = JSON.parseObject(after_json_obj.toJSONString(), DefaultMap.class);
+        DefaultMap defaultMap = tapUtils.fromJson(json);
+        DefaultMap after_map = tapUtils.fromJson(defaultMap.get("after").toString());
 
 
         try {
             ResultSet tables = conn.getMetaData().getTables(conn.getCatalog(), null, dorisConfig.getTable(), new String[]{"TABLE"});
 
             while (tables.next()) {
-                Assertions.assertEquals(dorisConfig.getTable(),tables.getString("TABLE_NAME"));
+                Assertions.assertEquals(dorisConfig.getTable(), tables.getString("TABLE_NAME"));
             }
             Class.forName(dorisConfig.getJdbcDriver());
 
@@ -51,7 +53,7 @@ public class ConnectionTestTest {
                 stmt.setString(1, after_map.getValue("id", null).toString());
                 stmt.setString(2, after_map.getValue("description", null));
                 stmt.setString(3, after_map.getValue("name", null));
-                BigDecimal age =  after_map.getValue("age", null);
+                BigDecimal age = after_map.getValue("age", null);
                 stmt.setDouble(4, age.doubleValue());
                 stmt.addBatch();
             }
@@ -61,11 +63,11 @@ public class ConnectionTestTest {
             String query = "select * from " + dorisConfig.getTable();
             stmt = conn.prepareStatement(query);
             ResultSet rs = stmt.executeQuery();
-            while(rs.next()){
-                Assertions.assertEquals(rs.getString(1),"1.0");
-                Assertions.assertEquals(rs.getString(2),"description123");
-                Assertions.assertEquals(rs.getString(3),"name123");
-                Assertions.assertEquals(rs.getDouble(4),12.0);
+            while (rs.next()) {
+                Assertions.assertEquals(rs.getString(1), "1.0");
+                Assertions.assertEquals(rs.getString(2), "description123");
+                Assertions.assertEquals(rs.getString(3), "name123");
+                Assertions.assertEquals(rs.getDouble(4), 12.0);
             }
 
             String truncate = "truncate table " + dorisConfig.getTable();
--- connectors/doris-connector/src/main/test/DiscoverSchemaTest.java
+++ connectors/doris-connector/src/main/test/DiscoverSchemaTest.java
@@ -1,14 +1,37 @@
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.tapdata.entity.schema.TapField;
+import io.tapdata.entity.schema.TapTable;
+import io.tapdata.pdk.apis.common.DefaultMap;
+import io.tapdata.pdk.apis.utils.ImplementationUtils;
+import io.tapdata.pdk.apis.utils.TapUtils;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+
 @DisplayName("Tests for discover schema")
 public class DiscoverSchemaTest {
+    private final TapUtils tapUtils = ImplementationUtils.getTapUtils();
 
     @Test
     @Disabled("Disabled")
     @DisplayName("Test method discoverSchema")
-    void discoverSchema() {
+    void discoverSchema() throws IOException {
+        String sourcePath = "B:\\code\\tapdata\\idaas-pdk\\connectors\\doris-connector\\src\\main\\resources\\source.json";
+        ObjectMapper mapper = new ObjectMapper(new JsonFactory());
+        DefaultMap source_map = mapper.readValue(new File(sourcePath), DefaultMap.class);
+        TapTable tapTable = new TapTable("empty_table");
+        for (Map.Entry<String, Object> entry : source_map.entrySet()) {
+            DefaultMap defaultMap = tapUtils.fromJson(tapUtils.toJson(entry.getValue()));
+            TapField tapField = new TapField(entry.getKey(), (String) defaultMap.get("originType"));
+            tapTable.add(tapField);
+        }
+        // TODO TAPTYPE SCHEMA -> DORIS SCHEMA
+
     }
 
 }
