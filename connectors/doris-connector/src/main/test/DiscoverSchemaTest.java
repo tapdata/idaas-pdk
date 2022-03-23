e28299934973288fec948d2a2bfd2013bccc7566
longshaofeng <345064577@qq.com>
2022-03-23 20:55:41 +0800

    add doris basic Test(connect and write)

---
+++ connectors/doris-connector/src/main/test/DiscoverSchemaTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Tests for discover schema")
+public class DiscoverSchemaTest {
+
+    @Test
+    @Disabled("Disabled")
+    @DisplayName("Test method discoverSchema")
+    void discoverSchema() {
+    }
+
+}
