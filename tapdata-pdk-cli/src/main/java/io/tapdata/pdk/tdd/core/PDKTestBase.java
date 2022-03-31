a2b93abd13d38e292a915abd5b6811d7870b6931
longshaofeng <345064577@qq.com>
2022-03-31 13:30:29 +0800

    add List equal in objectIsEqual

--- tapdata-pdk-cli/src/main/java/io/tapdata/pdk/tdd/core/PDKTestBase.java
+++ tapdata-pdk-cli/src/main/java/io/tapdata/pdk/tdd/core/PDKTestBase.java
@@ -28,10 +28,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
@@ -245,58 +242,13 @@ public class PDKTestBase {
         MapDifference<String, Object> difference = Maps.difference(firstRecord, result);
         Map<String, MapDifference.ValueDifference<Object>> differenceMap = difference.entriesDiffering();
         builder.append("Differences: \n");
-        boolean equalResult;
         boolean different = false;
         for(Map.Entry<String, MapDifference.ValueDifference<Object>> entry : differenceMap.entrySet()) {
-            equalResult = false;
             MapDifference.ValueDifference<Object> diff = entry.getValue();
-//            if(entry.getKey().equals("tapBinary")) {
-//            }
-            if((diff.leftValue() instanceof byte[]) && (diff.rightValue() instanceof byte[])) {
-                equalResult = Arrays.equals((byte[])diff.leftValue(), (byte[])diff.rightValue());
-            } else if((diff.leftValue() instanceof byte[]) && (diff.rightValue() instanceof String)) {
-                //byte[] vs string, base64 decode string
-                try {
-//                    byte[] rightBytes = Base64.getDecoder().decode((String) diff.rightValue());
-                    byte[] rightBytes = Base64.decodeBase64((String) diff.rightValue());
-                    equalResult = Arrays.equals((byte[])diff.leftValue(), rightBytes);
-                } catch(Throwable ignored) {}
-            } else if((diff.leftValue() instanceof Number) && (diff.rightValue() instanceof Number)) {
-                //number vs number, equal by value
-                BigDecimal leftB = null;
-                BigDecimal rightB = null;
-                if(diff.leftValue() instanceof BigDecimal) {
-                    leftB = (BigDecimal) diff.leftValue();
-                }
-                if(diff.rightValue() instanceof BigDecimal) {
-                    rightB = (BigDecimal) diff.rightValue();
-                }
-                if(leftB == null) {
-                    leftB = BigDecimal.valueOf(((Number)diff.leftValue()).doubleValue());
-                }
-                if(rightB == null) {
-                    rightB = BigDecimal.valueOf(((Number)diff.rightValue()).doubleValue());
-                }
-                equalResult = leftB.compareTo(rightB) == 0;
-            } else if((diff.leftValue() instanceof Boolean)) {
-                if(diff.rightValue() instanceof Number) {
-                    //boolean true == (!=0), false == 0
-                    Boolean leftBool = (Boolean) diff.leftValue();
-                    if(leftBool) {
-                        equalResult = ((Number)diff.rightValue()).longValue() != 0;
-                    } else {
-                        equalResult = ((Number)diff.rightValue()).longValue() == 0;
-                    }
-                } else if(diff.rightValue() instanceof String) {
-                    //boolean true == "true", false == "false"
-                    Boolean leftBool = (Boolean) diff.leftValue();
-                    if(leftBool) {
-                        equalResult = ((String)diff.rightValue()).equalsIgnoreCase("true");
-                    } else {
-                        equalResult = ((String)diff.rightValue()).equalsIgnoreCase("false");
-                    }
-                }
-            }
+            Object leftValue = diff.leftValue();
+            Object rightValue = diff.rightValue();
+
+            boolean equalResult = objectIsEqual(leftValue,rightValue);
 
             if(!equalResult) {
                 different = true;
@@ -308,6 +260,65 @@ public class PDKTestBase {
         return different;
     }
 
+    public boolean objectIsEqual(Object leftValue, Object rightValue) {
+        boolean equalResult = false;
+        if ((leftValue instanceof List) && (rightValue instanceof List)){
+            if(((List<?>) leftValue).size() == ((List<?>) rightValue).size()){
+                for (int i = 0; i < ((List<?>) leftValue).size(); i++) {
+                    equalResult = objectIsEqual(((List<?>) leftValue).get(i),((List<?>) rightValue).get(i));
+                    if(!equalResult) break;
+                }
+            }
+        }
+
+        if ((leftValue instanceof byte[]) && (rightValue instanceof byte[])) {
+            equalResult = Arrays.equals((byte[]) leftValue, (byte[]) rightValue);
+        } else if ((leftValue instanceof byte[]) && (rightValue instanceof String)) {
+            //byte[] vs string, base64 decode string
+            try {
+//                    byte[] rightBytes = Base64.getDecoder().decode((String) rightValue);
+                byte[] rightBytes = Base64.decodeBase64((String) rightValue);
+                equalResult = Arrays.equals((byte[]) leftValue, rightBytes);
+            } catch (Throwable ignored) {
+            }
+        } else if ((leftValue instanceof Number) && (rightValue instanceof Number)) {
+            //number vs number, equal by value
+            BigDecimal leftB = null;
+            BigDecimal rightB = null;
+            if (leftValue instanceof BigDecimal) {
+                leftB = (BigDecimal) leftValue;
+            }
+            if (rightValue instanceof BigDecimal) {
+                rightB = (BigDecimal) rightValue;
+            }
+            if (leftB == null) {
+                leftB = BigDecimal.valueOf(((Number) leftValue).doubleValue());
+            }
+            if (rightB == null) {
+                rightB = BigDecimal.valueOf(((Number) rightValue).doubleValue());
+            }
+            equalResult = leftB.compareTo(rightB) == 0;
+        } else if ((leftValue instanceof Boolean)) {
+            if (rightValue instanceof Number) {
+                //boolean true == (!=0), false == 0
+                Boolean leftBool = (Boolean) leftValue;
+                if (Boolean.TRUE.equals(leftBool)) {
+                    equalResult = ((Number) rightValue).longValue() != 0;
+                } else {
+                    equalResult = ((Number) rightValue).longValue() == 0;
+                }
+            } else if (rightValue instanceof String) {
+                //boolean true == "true", false == "false"
+                Boolean leftBool = (Boolean) leftValue;
+                if (Boolean.TRUE.equals(leftBool)) {
+                    equalResult = ((String) rightValue).equalsIgnoreCase("true");
+                } else {
+                    equalResult = ((String) rightValue).equalsIgnoreCase("false");
+                }
+            }
+        }
+        return equalResult;
+    }
 
     public TapConnector getTestConnector() {
         return testConnector;
