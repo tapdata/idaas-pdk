a155c5a45fed890489ce790cea2befa4f297d4aa
Leo <59272375+LoSofo@users.noreply.github.com>
2022-04-07 13:29:28 +0800

    init mongodb

---
+++ connectors/mongodb-connector/src/main/java/io/tapdata/mongodb/MongodbConnector.java
@@ -0,0 +1,235 @@
+package io.tapdata.mongodb;
+
+import io.tapdata.base.ConnectorBase;
+import io.tapdata.entity.codec.TapCodecRegistry;
+import io.tapdata.entity.event.dml.*;
+import io.tapdata.entity.event.dml.TapInsertRecordEvent;
+import io.tapdata.entity.event.dml.TapRecordEvent;
+import io.tapdata.entity.schema.TapTable;
+import io.tapdata.pdk.apis.TapConnector;
+import io.tapdata.pdk.apis.context.TapConnectionContext;
+import io.tapdata.pdk.apis.annotations.TapConnectorClass;
+import io.tapdata.pdk.apis.context.TapConnectorContext;
+import io.tapdata.pdk.apis.entity.FilterResult;
+import io.tapdata.pdk.apis.entity.TapFilter;
+import io.tapdata.pdk.apis.entity.TestItem;
+import io.tapdata.pdk.apis.entity.WriteListResult;
+import io.tapdata.pdk.apis.functions.ConnectorFunctions;
+import io.tapdata.pdk.apis.logger.PDKLogger;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+
+/**
+ * Different Connector need use different "spec.json" file with different pdk id which specified in Annotation "TapConnectorClass"
+ * In parent class "ConnectorBase", provides many simplified methods to develop connector
+ */
+@TapConnectorClass("spec.json")
+public class MongodbConnector extends ConnectorBase implements TapConnector {
+    public static final String TAG = MongodbConnector.class.getSimpleName();
+    private final AtomicLong counter = new AtomicLong();
+    private final AtomicBoolean isShutDown = new AtomicBoolean(false);
+
+    /**
+     * The method invocation life circle is below,
+     * initiated -> discoverSchema -> destroy -> ended
+     *
+     * You need to create the connection to your data source and release the connection in destroy method.
+     * In connectionContext, you can get the connection config which is the user input for your connection application, described in your json file.
+     *
+     * Consumer can accept multiple times, especially huge number of table list.
+     * This is sync method, once the method return, Incremental engine will consider schema has been discovered.
+     *
+     * @param connectionContext
+     * @param consumer
+     */
+    @Override
+    public void discoverSchema(TapConnectionContext connectionContext, Consumer<List<TapTable>> consumer) {
+        //TODO Load tables from database, connection information in connectionContext#getConnectionConfig
+        //Sample code shows how to define tables.
+        consumer.accept(list(
+                //Define first table
+                table("empty-table1"),
+                //Define second table
+                table("empty-table2"))
+        );
+    }
+
+    /**
+     * The method invocation life circle is below,
+     * initiated -> connectionTest -> destroy -> ended
+     *
+     * You need to create the connection to your data source and release the connection in destroy method.
+     * In connectionContext, you can get the connection config which is the user input for your connection application, described in your json file.
+     *
+     * consumer can call accept method multiple times to test different items
+     *
+     * @param connectionContext
+     * @return
+     */
+    @Override
+    public void connectionTest(TapConnectionContext connectionContext, Consumer<TestItem> consumer) {
+        //Assume below tests are successfully, below tests are recommended, but not required.
+        //Connection test
+        //TODO execute connection test here
+        consumer.accept(testItem(TestItem.ITEM_CONNECTION, TestItem.RESULT_SUCCESSFULLY));
+        //Login test
+        //TODO execute login test here
+        consumer.accept(testItem(TestItem.ITEM_LOGIN, TestItem.RESULT_SUCCESSFULLY));
+        //Read test
+        //TODO execute read test by checking role permission
+        consumer.accept(testItem(TestItem.ITEM_READ, TestItem.RESULT_SUCCESSFULLY));
+        //Write test
+        //TODO execute write test by checking role permission
+        consumer.accept(testItem(TestItem.ITEM_WRITE, TestItem.RESULT_SUCCESSFULLY));
+
+        //When test failed
+//        consumer.accept(testItem(TestItem.ITEM_CONNECTION, TestItem.RESULT_FAILED, "Connection refused"));
+        //When test successfully, but some warn is reported.
+//        consumer.accept(testItem(TestItem.ITEM_READ_LOG, TestItem.RESULT_SUCCESSFULLY_WITH_WARN, "CDC not enabled, please check your database settings"));
+    }
+
+    /**
+     * Register connector capabilities here.
+     *
+     * To be as a target, please implement WriteRecordFunction and QueryByFilterFunction.
+     * If the database need create table before record insertion, then please implement CreateTableFunction and DropTableFunction,
+     *  Incremental engine will generate the data types for each field base on incoming records for CreateTableFunction to create the table.
+     *
+     * If defined data types in spec.json is not covered all the TapValue,
+     * like TapTimeValue, TapMapValue, TapDateValue, TapArrayValue, TapYearValue, TapNumberValue, TapBooleanValue, TapDateTimeValue, TapBinaryValue, TapRawValue, TapStringValue,
+     * then please provide the custom codec for missing TapValue by using codeRegistry.
+     * This is only needed when database need create table before insert records.
+     *
+     * @param connectorFunctions
+     * @param codecRegistry
+     */
+    @Override
+    public void registerCapabilities(ConnectorFunctions connectorFunctions, TapCodecRegistry codecRegistry) {
+        connectorFunctions.supportWriteRecord(this::writeRecord);
+        connectorFunctions.supportQueryByFilter(this::queryByFilter);
+
+        //If database need insert record before table created, then please implement the below two methods.
+//        connectorFunctions.supportCreateTable(this::createTable);
+//        connectorFunctions.supportDropTable(this::dropTable);
+
+        //If database need insert record before table created, please implement the custom codec for the TapValue that data types in spec.json didn't cover.
+        //TapTimeValue, TapMapValue, TapDateValue, TapArrayValue, TapYearValue, TapNumberValue, TapBooleanValue, TapDateTimeValue, TapBinaryValue, TapRawValue, TapStringValue
+//        codecRegistry.registerFromTapValue(TapRawValue.class, "text", tapRawValue -> {
+//            if (tapRawValue != null && tapRawValue.getValue() != null)
+//                return toJson(tapRawValue.getValue());
+//            return "null";
+//        });
+
+
+        //Below capabilities, developer can decide to implement or not.
+//        connectorFunctions.supportAlterTable(this::alterTable);
+//        connectorFunctions.supportClearTable(this::clearTable);
+
+//        connectorFunctions.supportBatchRead(this::batchRead);
+//        connectorFunctions.supportStreamRead(this::streamRead);
+//        connectorFunctions.supportBatchCount(this::batchCount);
+//        connectorFunctions.supportBatchOffset(this::batchOffset);
+//        connectorFunctions.supportStreamOffset(this::streamOffset);
+
+
+    }
+
+    /**
+     * The method invocation life circle is below,
+     * initiated ->
+     *  if(needCreateTable)
+     *      createTable
+     *  if(needClearTable)
+     *      clearTable
+     *  if(needDropTable)
+     *      dropTable
+     *  writeRecord
+     * -> destroy -> ended
+     *
+     * @param connectorContext
+     * @param tapRecordEvents
+     * @param writeListResultConsumer
+     */
+    private void writeRecord(TapConnectorContext connectorContext, List<TapRecordEvent> tapRecordEvents, Consumer<WriteListResult<TapRecordEvent>> writeListResultConsumer) {
+        //TODO write records into database
+
+        //Below is sample code to print received events which suppose to write to database.
+        AtomicLong inserted = new AtomicLong(0); //insert count
+        AtomicLong updated = new AtomicLong(0); //update count
+        AtomicLong deleted = new AtomicLong(0); //delete count
+        for(TapRecordEvent recordEvent : tapRecordEvents) {
+            if(recordEvent instanceof TapInsertRecordEvent) {
+                //TODO insert record
+                inserted.incrementAndGet();
+                PDKLogger.info(TAG, "Record Write TapInsertRecordEvent {}", toJson(recordEvent));
+            } else if(recordEvent instanceof TapUpdateRecordEvent) {
+                //TODO update record
+                updated.incrementAndGet();
+                PDKLogger.info(TAG, "Record Write TapUpdateRecordEvent {}", toJson(recordEvent));
+            } else if(recordEvent instanceof TapDeleteRecordEvent) {
+                //TODO delete record
+                deleted.incrementAndGet();
+                PDKLogger.info(TAG, "Record Write TapDeleteRecordEvent {}", toJson(recordEvent));
+            }
+        }
+        //Need to tell incremental engine the write result
+        writeListResultConsumer.accept(writeListResult()
+                .insertedCount(inserted.get())
+                .modifiedCount(updated.get())
+                .removedCount(deleted.get()));
+    }
+
+    /**
+     * The method will mainly be used by TDD tests. To verify the record has writen correctly or not.
+     *
+     * @param connectorContext
+     * @param filters Multple fitlers, need return multiple filter results
+     * @param listConsumer tell incremental engine the filter results according to filters
+     */
+    private void queryByFilter(TapConnectorContext connectorContext, List<TapFilter> filters, Consumer<List<FilterResult>> listConsumer){
+        //Filter is exactly match.
+        //If query by the filter, no value is in database, please still create a FitlerResult with null value in it. So that incremental engine can understand the filter has no value.
+
+        //TODO Implement the query by filter
+//        if(filters != null) {
+//            List<FilterResult> filterResults = new ArrayList<>();
+//            for(TapFilter filter : filters) {
+//                Map<String, Object> value = primaryKeyRecordMap.get(primaryKey(connectorContext, filter.getMatch()));
+//                FilterResult filterResult = new FilterResult().result(value).filter(filter);
+//                filterResults.add(filterResult);
+//            }
+//            listConsumer.accept(filterResults);
+//        }
+
+    }
+
+    /**
+     * Generate primary values as a string, used for KV database as primary key.
+     * This method is only FYI.
+     */
+    private String primaryKey(TapConnectorContext connectorContext, Map<String, Object> value) {
+        Collection<String> primaryKeys = connectorContext.getTable().primaryKeys();
+        StringBuilder builder = new StringBuilder();
+        for(String primaryKey : primaryKeys) {
+            builder.append(value.get(primaryKey));
+        }
+        return builder.toString();
+    }
+
+    /**
+     * The method invocation life circle is below,
+     * initiated -> sourceFunctions/targetFunctions -> destroy -> ended
+     *
+     * In connectorContext,
+     * you can get the connection/node config which is the user input for your connection/node application, described in your json file.
+     * current instance is serving for the table from connectorContext.
+     */
+    @Override
+    public void destroy() {
+        //TODO release resources
+        isShutDown.set(true);
+    }
+}
