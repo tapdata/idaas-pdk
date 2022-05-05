64df6c569de39ac5ba8212b76fa13c9ef73e674e
supercoolgj <coolgj@163.com>
2022-05-05 10:14:59 +0800

    调用mavenCLI编译问题解决，maven的repository路径问题

--- tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/TDDMysqlMain.java
+++ tapdata-pdk-cli/src/main/java/io/tapdata/pdk/cli/TDDMysqlMain.java
@@ -13,7 +13,8 @@ public class TDDMysqlMain {
     public static void main(String... args) {
         args = new String[]{"test", "-c", "tapdata-pdk-cli/src/main/resources/config/mysql.json",
 //                "-t", "io.tapdata.pdk.tdd.tests.target.DMLTest",
-                "dist/mysql8-connector-v1.0-SNAPSHOT.jar"
+                "connectors/mysql8-connector"
+//                "dist/mysql8-connector-v1.0-SNAPSHOT.jar"
         };
         Main.registerCommands().parseWithHandler(new CommandLine.RunLast(), args);
     }
