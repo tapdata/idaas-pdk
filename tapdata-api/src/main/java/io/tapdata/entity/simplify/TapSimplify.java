57a606f309c8ee656d64073b5f56a82abfdab786
samuel <sam@tapdata.io>
2022-05-31 18:01:37 +0800

    TapSimplify add formatTapDateTime function

--- tapdata-api/src/main/java/io/tapdata/entity/simplify/TapSimplify.java
+++ tapdata-api/src/main/java/io/tapdata/entity/simplify/TapSimplify.java
@@ -19,221 +19,234 @@ import io.tapdata.entity.utils.*;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class TapSimplify {
-    private static final TapUtils tapUtils = InstanceFactory.instance(TapUtils.class);
-    private static final JsonParser jsonParser = InstanceFactory.instance(JsonParser.class);
+	private static final TapUtils tapUtils = InstanceFactory.instance(TapUtils.class);
+	private static final JsonParser jsonParser = InstanceFactory.instance(JsonParser.class);
 
-    public static void interval(Runnable runnable, int seconds) {
-        tapUtils.interval(runnable, seconds);
-    }
-
-
-    public static String toJsonWithClass(Object obj) {
-        return jsonParser.toJsonWithClass(obj);
-    }
-
-    public static Object fromJsonWithClass(String json) {
-        return jsonParser.fromJsonWithClass(json);
-    }
+	public static void interval(Runnable runnable, int seconds) {
+		tapUtils.interval(runnable, seconds);
+	}
 
 
-    public static String toJson(Object obj, JsonParser.ToJsonFeature... features) {
-        return jsonParser.toJson(obj, features);
-    }
+	public static String toJsonWithClass(Object obj) {
+		return jsonParser.toJsonWithClass(obj);
+	}
 
-    public static DataMap fromJson(String json) {
-        return jsonParser.fromJson(json);
-    }
+	public static Object fromJsonWithClass(String json) {
+		return jsonParser.fromJsonWithClass(json);
+	}
 
-    public static <T> T fromJson(String json, Class<T> clazz) {
-        return jsonParser.fromJson(json, clazz);
-    }
 
-    public static String format(String message, Object... args) {
-        return FormatUtils.format(message, args);
-    }
+	public static String toJson(Object obj, JsonParser.ToJsonFeature... features) {
+		return jsonParser.toJson(obj, features);
+	}
 
-    public static TapField field(String name, String type) {
-        return new TapField(name, type);
-    }
+	public static DataMap fromJson(String json) {
+		return jsonParser.fromJson(json);
+	}
 
-    public static TapTable table(String tableName, String id) {
-        return new TapTable(tableName, id);
-    }
+	public static <T> T fromJson(String json, Class<T> clazz) {
+		return jsonParser.fromJson(json, clazz);
+	}
 
-    public static TapTable table(String nameAndId) {
-        return new TapTable(nameAndId);
-    }
+	public static String format(String message, Object... args) {
+		return FormatUtils.format(message, args);
+	}
 
-    public static TapString tapString() {
-        return new TapString();
-    }
+	public static TapField field(String name, String type) {
+		return new TapField(name, type);
+	}
 
-    public static TapNumber tapNumber() {
-        return new TapNumber();
-    }
+	public static TapTable table(String tableName, String id) {
+		return new TapTable(tableName, id);
+	}
 
-    public static TapRaw tapRaw() {
-        return new TapRaw();
-    }
+	public static TapTable table(String nameAndId) {
+		return new TapTable(nameAndId);
+	}
 
-    public static TapArray tapArray() {
-        return new TapArray();
-    }
+	public static TapString tapString() {
+		return new TapString();
+	}
 
-    public static TapMap tapMap() {
-        return new TapMap();
-    }
+	public static TapNumber tapNumber() {
+		return new TapNumber();
+	}
 
-    public static TapYear tapYear() {
-        return new TapYear();
-    }
+	public static TapRaw tapRaw() {
+		return new TapRaw();
+	}
 
-    public static TapDate tapDate() {
-        return new TapDate();
-    }
+	public static TapArray tapArray() {
+		return new TapArray();
+	}
 
-    public static TapBoolean tapBoolean() {
-        return new TapBoolean();
-    }
+	public static TapMap tapMap() {
+		return new TapMap();
+	}
 
-    public static TapBinary tapBinary() {
-        return new TapBinary();
-    }
+	public static TapYear tapYear() {
+		return new TapYear();
+	}
 
-    public static TapTime tapTime() {
-        return new TapTime();
-    }
+	public static TapDate tapDate() {
+		return new TapDate();
+	}
 
-    public static TapDateTime tapDateTime() {
-        return new TapDateTime();
-    }
+	public static TapBoolean tapBoolean() {
+		return new TapBoolean();
+	}
 
-    public static Entry entry(String key, Object value) {
-        return new Entry(key, value);
-    }
+	public static TapBinary tapBinary() {
+		return new TapBinary();
+	}
 
-    public static <T> List<T> list(T... ts) {
-        return new ArrayList<>(Arrays.asList(ts));
-    }
+	public static TapTime tapTime() {
+		return new TapTime();
+	}
 
-    public static <T> List<T> list() {
-        return new ArrayList<T>();
-    }
+	public static TapDateTime tapDateTime() {
+		return new TapDateTime();
+	}
 
-    public static Map<String, Object> map() {
-        return new LinkedHashMap<>();
-    }
+	public static Entry entry(String key, Object value) {
+		return new Entry(key, value);
+	}
 
-    public static Map<String, Object> map(Entry... entries) {
-        Map<String, Object> map = new LinkedHashMap<>();
-        if (entries != null) {
-            for (Entry entry : entries) {
-                if (entry.getKey() != null && entry.getValue() != null)
-                    map.put(entry.getKey(), entry.getValue());
-            }
-        }
-        return map;
-    }
+	public static <T> List<T> list(T... ts) {
+		return new ArrayList<>(Arrays.asList(ts));
+	}
 
-    public static TapDropTableEvent dropTableEvent(String tableId) {
-        TapDropTableEvent dropTableEvent = new TapDropTableEvent();
-        dropTableEvent.setTime(System.currentTimeMillis());
-        dropTableEvent.setTableId(tableId);
-        return dropTableEvent;
-    }
+	public static <T> List<T> list() {
+		return new ArrayList<T>();
+	}
 
-    public static TapCreateTableEvent createTableEvent(TapTable table) {
-        TapCreateTableEvent createTableEvent = new TapCreateTableEvent();
-        createTableEvent.setTime(System.currentTimeMillis());
-        createTableEvent.setTable(table);
-        createTableEvent.setTableId(table.getId());
-        return createTableEvent;
-    }
+	public static Map<String, Object> map() {
+		return new LinkedHashMap<>();
+	}
 
-    public static TapClearTableEvent clearTableEvent(String tableId) {
-        TapClearTableEvent clearTableEvent = new TapClearTableEvent();
-        clearTableEvent.setTime(System.currentTimeMillis());
-        clearTableEvent.setTableId(tableId);
-        return clearTableEvent;
-    }
+	public static Map<String, Object> map(Entry... entries) {
+		Map<String, Object> map = new LinkedHashMap<>();
+		if (entries != null) {
+			for (Entry entry : entries) {
+				if (entry.getKey() != null && entry.getValue() != null)
+					map.put(entry.getKey(), entry.getValue());
+			}
+		}
+		return map;
+	}
 
-    public static TapAlterTableEvent alterTableEvent(String tableId) {
-        TapAlterTableEvent alterTableEvent = new TapAlterTableEvent();
-        alterTableEvent.setTime(System.currentTimeMillis());
-        alterTableEvent.setTableId(tableId);
-        return alterTableEvent;
-    }
+	public static TapDropTableEvent dropTableEvent(String tableId) {
+		TapDropTableEvent dropTableEvent = new TapDropTableEvent();
+		dropTableEvent.setTime(System.currentTimeMillis());
+		dropTableEvent.setTableId(tableId);
+		return dropTableEvent;
+	}
 
-    public static TapCreateIndexEvent createIndexEvent(String tableId, List<TapIndex> indexList) {
-        TapCreateIndexEvent createIndexEvent = new TapCreateIndexEvent();
-        createIndexEvent.setTime(System.currentTimeMillis());
-        createIndexEvent.setTableId(tableId);
-        createIndexEvent.setIndexList(indexList);
-        return createIndexEvent;
-    }
+	public static TapCreateTableEvent createTableEvent(TapTable table) {
+		TapCreateTableEvent createTableEvent = new TapCreateTableEvent();
+		createTableEvent.setTime(System.currentTimeMillis());
+		createTableEvent.setTable(table);
+		createTableEvent.setTableId(table.getId());
+		return createTableEvent;
+	}
 
-    public static TapDeleteIndexEvent deleteIndexEvent(String tableId, List<String> indexNames) {
-        TapDeleteIndexEvent deleteIndexEvent = new TapDeleteIndexEvent();
-        deleteIndexEvent.setTableId(tableId);
-        deleteIndexEvent.setTime(System.currentTimeMillis());
-        deleteIndexEvent.setIndexNames(indexNames);
-        return deleteIndexEvent;
-    }
+	public static TapClearTableEvent clearTableEvent(String tableId) {
+		TapClearTableEvent clearTableEvent = new TapClearTableEvent();
+		clearTableEvent.setTime(System.currentTimeMillis());
+		clearTableEvent.setTableId(tableId);
+		return clearTableEvent;
+	}
 
-    public static TapInsertRecordEvent insertRecordEvent(Map<String, Object> after, String table) {
-        return new TapInsertRecordEvent().init().after(after).table(table);
-    }
+	public static TapAlterTableEvent alterTableEvent(String tableId) {
+		TapAlterTableEvent alterTableEvent = new TapAlterTableEvent();
+		alterTableEvent.setTime(System.currentTimeMillis());
+		alterTableEvent.setTableId(tableId);
+		return alterTableEvent;
+	}
 
-    public static TapDeleteRecordEvent deleteDMLEvent(Map<String, Object> before, String table) {
-        return new TapDeleteRecordEvent().init().before(before).table(table);
-    }
+	public static TapCreateIndexEvent createIndexEvent(String tableId, List<TapIndex> indexList) {
+		TapCreateIndexEvent createIndexEvent = new TapCreateIndexEvent();
+		createIndexEvent.setTime(System.currentTimeMillis());
+		createIndexEvent.setTableId(tableId);
+		createIndexEvent.setIndexList(indexList);
+		return createIndexEvent;
+	}
 
-    public static TapUpdateRecordEvent updateDMLEvent(Map<String, Object> before, Map<String, Object> after, String table) {
-        return new TapUpdateRecordEvent().init().before(before).after(after).table(table);
-    }
+	public static TapDeleteIndexEvent deleteIndexEvent(String tableId, List<String> indexNames) {
+		TapDeleteIndexEvent deleteIndexEvent = new TapDeleteIndexEvent();
+		deleteIndexEvent.setTableId(tableId);
+		deleteIndexEvent.setTime(System.currentTimeMillis());
+		deleteIndexEvent.setIndexNames(indexNames);
+		return deleteIndexEvent;
+	}
 
-    public static HeartbeatEvent heartbeatEvent(Long referenceTime) {
-        return new HeartbeatEvent().init().referenceTime(referenceTime);
-    }
+	public static TapInsertRecordEvent insertRecordEvent(Map<String, Object> after, String table) {
+		return new TapInsertRecordEvent().init().after(after).table(table);
+	}
 
-    public static void sleep(long milliseconds) {
-        if (milliseconds < 0)
-            return;
-        try {
-            Thread.sleep(milliseconds);
-        } catch (InterruptedException interruptedException) {
-            interruptedException.printStackTrace();
-        }
-    }
+	public static TapDeleteRecordEvent deleteDMLEvent(Map<String, Object> before, String table) {
+		return new TapDeleteRecordEvent().init().before(before).table(table);
+	}
 
-    public static Object convertDateTimeToDate(DateTime dateTime) {
-        if (dateTime != null) {
-            Long milliseconds;
-            Integer nano = dateTime.getNano();
-            Long seconds = dateTime.getSeconds();
-            if (seconds != null) {
-                milliseconds = seconds * 1000;
-                if (nano != null) {
-                    milliseconds += milliseconds + (nano / 1000 / 1000);
-                }
-            } else {
-                return null;
-            }
-            return new Date(milliseconds);
-        }
-        return null;
-    }
+	public static TapUpdateRecordEvent updateDMLEvent(Map<String, Object> before, Map<String, Object> after, String table) {
+		return new TapUpdateRecordEvent().init().before(before).after(after).table(table);
+	}
 
-    public static String getStackString(Throwable throwable) {
-        StringWriter sw = new StringWriter();
-        try (
-                PrintWriter pw = new PrintWriter(sw)
-        ) {
-            throwable.printStackTrace(pw);
-            return sw.toString();
-        }
-    }
+	public static HeartbeatEvent heartbeatEvent(Long referenceTime) {
+		return new HeartbeatEvent().init().referenceTime(referenceTime);
+	}
+
+	public static void sleep(long milliseconds) {
+		if (milliseconds < 0)
+			return;
+		try {
+			Thread.sleep(milliseconds);
+		} catch (InterruptedException interruptedException) {
+			interruptedException.printStackTrace();
+		}
+	}
+
+	public static Object convertDateTimeToDate(DateTime dateTime) {
+		if (dateTime != null) {
+			Long milliseconds;
+			Integer nano = dateTime.getNano();
+			Long seconds = dateTime.getSeconds();
+			if (seconds != null) {
+				milliseconds = seconds * 1000;
+				if (nano != null) {
+					milliseconds += milliseconds + (nano / 1000 / 1000);
+				}
+			} else {
+				return null;
+			}
+			return new Date(milliseconds);
+		}
+		return null;
+	}
+
+	public static String getStackString(Throwable throwable) {
+		StringWriter sw = new StringWriter();
+		try (
+				PrintWriter pw = new PrintWriter(sw)
+		) {
+			throwable.printStackTrace(pw);
+			return sw.toString();
+		}
+	}
+
+	public static String formatTapDateTime(DateTime dateTime, String pattern) {
+		try {
+			DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern);
+			LocalDateTime localDateTime = LocalDateTime.ofInstant(dateTime.toInstant(), ZoneId.of("GMT"));
+			return dateTimeFormatter.format(localDateTime);
+		} catch (Throwable e) {
+			throw new RuntimeException("Parse date time " + dateTime + " pattern " + pattern + ", failed, " + e.getMessage(), e);
+		}
+	}
 }
