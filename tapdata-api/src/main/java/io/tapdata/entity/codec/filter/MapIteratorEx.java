f66d2074dbcb8328acdcfb7fbabfa57efe204146
Aplomb <aplomb.chen@gmail.com>
2022-06-08 17:45:46 +0800

    Use constants for MapIteratorEx.

--- tapdata-api/src/main/java/io/tapdata/entity/codec/filter/MapIteratorEx.java
+++ tapdata-api/src/main/java/io/tapdata/entity/codec/filter/MapIteratorEx.java
@@ -2,9 +2,9 @@ package io.tapdata.entity.codec.filter;
 
 import java.util.Map;
 import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
 
 public interface MapIteratorEx {
+    String MAP_KEY_SEPARATOR = ".";
+    String ARRAY_KEY_SEPARATOR = ".#";
     void iterate(Map<String, Object> map, BiFunction<String, Object, Object> filter);
 }
