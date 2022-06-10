2e51469ac5ef31a676f35c4cf7448b9a75337a46
Aplomb <aplomb.chen@gmail.com>
2022-06-10 18:43:26 +0800

    Correct JavaTypeToTaptypes

---
+++ tapdata-tests/src/main/test/io/tapdata/pdk/core/api/impl/TapUtilsImplTest.java
@@ -0,0 +1,93 @@
+package io.tapdata.pdk.core.api.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import io.tapdata.entity.codecs.TDDUser;
+import io.tapdata.entity.utils.InstanceFactory;
+import io.tapdata.entity.utils.ObjectSerializable;
+import io.tapdata.entity.utils.TapUtils;
+import org.bson.BsonTimestamp;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TapUtilsImplTest {
+    @Test
+    public void test() {
+        TapUtils tapUtils = InstanceFactory.instance(TapUtils.class);
+
+        TDDUser user = new TDDUser();
+        user.setUserId("abc");
+        user.setName("aaa");
+
+        List<TDDUser> users = new ArrayList<>();
+        users.add(user);
+
+        Map<String, Object> userMap = new HashMap<>();
+        userMap.put("aaa", user);
+        userMap.put("bbb", user);
+        Map<String, Object> newUserMap = tapUtils.cloneMap(userMap);
+        assertEquals(userMap.size(), newUserMap.size());
+        assertEquals(((TDDUser)userMap.get("aaa")).getName(), ((TDDUser)newUserMap.get("aaa")).getName());
+        assertEquals(((TDDUser)userMap.get("bbb")).getName(), ((TDDUser)newUserMap.get("bbb")).getName());
+
+//        Document document = new Document();
+//        BsonTimestamp bsonTimestamp = new BsonTimestamp(new Date().getTime());
+//        ObjectId objectId = ObjectId.get();
+//        document.put("timestamp", bsonTimestamp);
+//        document.put("oid", objectId);
+//        Document embeddedDocument = new Document();
+//        embeddedDocument.put("aaa", 123);
+//        document.put("doc", embeddedDocument);
+//        Document newDocument = (Document) tapUtils.cloneMap(document);
+//        assertEquals(((BsonTimestamp)newDocument.get("timestamp")).getValue(), bsonTimestamp.getValue());
+//        assertEquals(newDocument.get("oid").toString(), objectId.toString());
+//        assertEquals(((Document)newDocument.get("doc")).get("aaa"), 123);
+//
+//        List<Document> documents = new ArrayList<>();
+//        documents.add(document);
+//        List<Document> newDocuments = (List<Document>) objectSerializable.toObject(objectSerializable.fromObject(documents));
+//        assertEquals(documents.size(), newDocuments.size());
+//        Document doc1 = newDocuments.get(0);
+//        assertEquals(((BsonTimestamp)doc1.get("timestamp")).getValue(), bsonTimestamp.getValue());
+//        assertEquals(doc1.get("oid").toString(), objectId.toString());
+//        assertEquals(((Document)doc1.get("doc")).get("aaa"), 123);
+//
+//        Map<TDDUser, Document> documentMap = new HashMap<>();
+//        documentMap.put(user, document);
+//        Map<TDDUser, Document> newDocumentMap = (Map<TDDUser, Document>) objectSerializable.toObject(objectSerializable.fromObject(documentMap));
+//        assertEquals(documentMap.size(), newDocumentMap.size());
+//        TDDUser userKey = documentMap.keySet().stream().findFirst().get();
+//        assertEquals(user.getName(), userKey.getName());
+//        Document doc2 = documentMap.get(userKey);
+//        assertEquals(((BsonTimestamp)doc2.get("timestamp")).getValue(), bsonTimestamp.getValue());
+//        assertEquals(doc2.get("oid").toString(), objectId.toString());
+//        assertEquals(((Document)doc2.get("doc")).get("aaa"), 123);
+//
+//        JSONObject jsonObject = new JSONObject();
+//        jsonObject.put("aaa", "BBB");
+//        JSONObject newJsonObject = (JSONObject) objectSerializable.toObject(objectSerializable.fromObject(jsonObject));
+//        assertEquals(jsonObject.get("aaa"), newJsonObject.get("aaa"));
+//
+//        JSONObject jsonObject1 = new JSONObject();
+//        jsonObject1.put("aaa", "BBB");
+//        jsonObject1.put("documentMap", documentMap);
+//        jsonObject1.put("documents", documents);
+//        jsonObject1.put("userMap", userMap);
+//        jsonObject1.put("users", users);
+//        jsonObject1.put("document", document);
+//        JSONObject newJsonObject1 = (JSONObject) objectSerializable.toObject(objectSerializable.fromObject(jsonObject1));
+//        assertEquals(jsonObject1.get("aaa"), newJsonObject1.get("aaa"));
+//        TDDUser userKey1 = ((Map<TDDUser, Document>)newJsonObject1.get("documentMap")).keySet().stream().findFirst().get();
+//        assertEquals(user.getName(), userKey1.getName());
+//        Document doc3 = documentMap.get(userKey);
+//        assertEquals(((BsonTimestamp)doc3.get("timestamp")).getValue(), bsonTimestamp.getValue());
+//        assertEquals(doc3.get("oid").toString(), objectId.toString());
+//        assertEquals(((Document)doc3.get("doc")).get("aaa"), 123);
+
+    }
+
+}
