b10b058046d80ada6dac7567cd0ab1b8f3bd5568
jarad <coolgj@163.com>
2022-06-15 17:47:06 +0800

    Oracle spec fix

--- connectors/connector-core/src/main/java/io/tapdata/kit/StringKit.java
+++ connectors/connector-core/src/main/java/io/tapdata/kit/StringKit.java
@@ -1,5 +1,7 @@
 package io.tapdata.kit;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -47,8 +49,27 @@ public class StringKit {
         return list.stream().map(s -> around + s + around).collect(Collectors.joining(splitter));
     }
 
-//    public static int compareVersion(String version1, String version2) {
-//        version1.split(".")
-//    }
+    public static int compareVersion(String version1, String version2) {
+        List<String> list1 = Arrays.stream(version1.split("\\.")).collect(Collectors.toList());
+        List<String> list2 = Arrays.stream(version2.split("\\.")).collect(Collectors.toList());
+        Iterator<String> iterator1 = list1.iterator();
+        Iterator<String> iterator2 = list2.iterator();
+        while (iterator1.hasNext() && iterator2.hasNext()) {
+            String str1 = iterator1.next();
+            String str2 = iterator2.next();
+            if (Integer.parseInt(str1) > Integer.parseInt(str2)) {
+                return 1;
+            } else if (Integer.parseInt(str1) < Integer.parseInt(str2)) {
+                return -1;
+            }
+        }
+        if (iterator1.hasNext()) {
+            return 1;
+        } else if (iterator2.hasNext()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 
 }
